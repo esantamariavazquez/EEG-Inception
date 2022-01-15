01a5edfcd5662371019c92fe18c58d65889bb812
Eduardo Santamaría-Vázquez <edusan1213@gmail.com>
2022-01-15 13:35:16 +0100

    Initial commit

---
+++ EEGInception_v1/test.py
@@ -0,0 +1,85 @@
+"""
+Usage example of EEG-Inception
+
+Download the dataset from:
+https://www.kaggle.com/esantamaria/gibuva-erpbci-dataset
+
+"""
+
+#%% IMPORT LIBRARIES
+import numpy as np
+import h5py, os
+import utils
+from EEGInception_v1 import EEGInceptionV1
+from tensorflow.keras.callbacks import EarlyStopping
+
+#%% PARAMETERS
+
+dataset_path = 'PATH TO DATASET (hdf5 file)'
+
+#%% HYPERPARAMETERS
+
+input_time = 1000
+fs = 128
+n_cha = 8
+filters_per_branch = 8
+scales_time = (500, 250, 125)
+dropout_rate = 0.25
+activation = 'elu'
+n_classes = 2
+learning_rate = 0.001
+
+#%% LOAD DATASET
+hf = h5py.File(dataset_path, 'r')
+features = np.array(hf.get("features"))
+erp_labels = np.array(hf.get("erp_labels"))
+codes = np.array(hf.get("codes"))
+trials = np.array(hf.get("trials"))
+sequences = np.array(hf.get("sequences"))
+matrix_indexes = np.array(hf.get("matrix_indexes"))
+run_indexes = np.array(hf.get("run_indexes"))
+subjects = np.array(hf.get("subjects"))
+database_ids = np.array(hf.get("database_ids"))
+target = np.array(hf.get("target"))
+matrix_dims = np.array(hf.get("matrix_dims"))
+hf.close()
+
+# Reshape epochs for EEG-Inception
+features = features.reshape(
+    (features.shape[0], features.shape[1],
+     features.shape[2], 1)
+)
+
+# One hot encoding of labels
+train_erp_labels = utils.one_hot_labels(erp_labels)
+
+#%%  TRAINING
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+
+# Create model
+model = EEGInceptionV1(
+    input_time=1000, fs=128, ncha=8, filters_per_branch=8,
+    scales_time=(500, 250, 125), dropout_rate=0.25,
+    activation='elu', n_classes=2, learning_rate=0.001)
+
+# Print model summary
+model.summary()
+
+# Callbacks
+early_stopping = EarlyStopping(
+    monitor='val_loss', min_delta=0.0001,
+    mode='min', patience=10, verbose=1,
+    restore_best_weights=True)
+
+# Fit model
+fit_hist = model.fit(features,
+                     erp_labels,
+                     epochs=500,
+                     batch_size=2048,
+                     validation_split=0.2,
+                     callbacks=[early_stopping])
+
+# Save
+model.save('model')
+
+
